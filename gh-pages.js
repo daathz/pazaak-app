ee67eb0d98ae939a00a49bba1f07b0d531793e27
Viktor Novák <novk.viktor@gmail.com>
2022-06-06 21:43:13 +0200

    initial gh pages commit

---
+++ gh-pages.js
@@ -0,0 +1,14 @@
+var ghpages = require('gh-pages');
+
+ghpages.publish(
+    'public',
+    {
+        branch: 'gh-pages',
+        repo: 'https://github.com/daathz/pazaak-app.git',
+        user: {
+            name: 'Viktor Novák',
+            email: 'novk.viktor@gmail.com'
+        }
+    },
+    () => console.log('Deploy complete!')
+)
